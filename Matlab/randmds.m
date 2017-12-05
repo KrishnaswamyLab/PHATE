4f94fcb6d7f9d68f5368cc930ee1813ee635477c
Kevin Moon <kevimoon@gmail.com>
2017-12-05 11:46:24 -0500

    Update randmds.m

--- Matlab/randmds.m
+++ Matlab/randmds.m
@@ -2,8 +2,8 @@ function Y = randmds(X, ndim)
 % Y = randmds(X, ndim)
 %    CMDS using random SVD
 
-X = X.^2; % ???????
+X = X.^2; % 
 X = bsxfun(@minus, X, mean(X,1));
 X = bsxfun(@minus, X, mean(X,2));
 [Y,~,~] = randPCA(X', ndim);
-%Y = X * U;
+
