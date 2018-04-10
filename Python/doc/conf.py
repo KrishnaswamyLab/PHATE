7c6e15bcaabedd677a5305dccb9280edb93b5390
mattamodio <matthewamodio@Matthews-MacBook-Pro.local>
2018-04-10 11:13:38 -0400

    moving files

--- Python/doc/conf.py
+++ Python/doc/conf.py
@@ -20,7 +20,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../phate'))
-
+import phate
+print(phate.phate)
 
 # -- General configuration ------------------------------------------------
 
