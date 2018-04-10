bf59072f54252e138fc76356df9ce28297ee0e13
Scott Gigante <scottgigante@gmail.com>
2018-04-10 16:10:12 -0400

    re-add ystatic

--- Python/doc/conf.py
+++ Python/doc/conf.py
@@ -100,7 +100,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['']
+html_static_path = ['ystatic']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
