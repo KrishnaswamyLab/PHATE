6e056c2445d3bdc09dbb6676db8d9a84017b1ff0
Scott Gigante <scottgigante@gmail.com>
2018-04-18 12:00:45 -0400

    allow library size norm for large sparse matrices

--- Python/phate/preprocessing.py
+++ Python/phate/preprocessing.py
@@ -32,18 +32,37 @@ def library_size_normalize(data, verbose=False):
         print("Normalizing library sizes for %s cells" % (data.shape[0]))
 
     try:
-        if isinstance(data, pd.core.sparse.frame.SparseDataFrame):
-            data = data.to_coo()
-        elif isinstance(data, pd.DataFrame):
-            data = np.array(data)
+        if pandas.api.types.is_sparse(data):
+            data = sparse.csr_matrix(data.to_coo())
     except NameError:
         pass
     median_transcript_count = np.median(np.array(data.sum(axis=1)))
-    try:
+    if sparse.issparse(data) and data.nnz >= 2**31:
+        # check we can access elements by index
+        try:
+            data[0, 0]
+        except TypeError:
+            data = sparse.csr_matrix(data)
+        # normalize in chunks - sklearn doesn't does with more
+        # than 2**31 non-zero elements
+        #
+        # determine maximum chunk size
+        split = 2**30 // (data.nnz // data.shape[0])
+        size_ok = False
+        while not size_ok:
+            for i in range(0, data.shape[0], split):
+                if data[i:i + split, :].nnz >= 2**31:
+                    split = split // 2
+                    break
+            size_ok = True
+        # normalize
+        data_norm = []
+        for i in range(0, data.shape[0], split):
+            data_norm.append(normalize(data[i:i + split, :], 'l1', axis=1))
+        # combine chunks
+        data_norm = sparse.vstack(data_norm)
+    else:
         data_norm = normalize(data, norm='l1', axis=1)
-    except (ValueError, MemoryError):
-        data_norm = sparse.vstack([normalize(
-            data[i, :], 'l1', axis=1) for i in range(data.shape[0])])
 
     # norm = 'l1' computes the L1 norm which computes the
     # axis = 1 independently normalizes each sample
