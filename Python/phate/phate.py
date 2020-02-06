fix n_eigs calculation

Python/phate/phate.py
@@ -1130,7 +1130,7 @@ class PHATE(BaseEstimator):
         t = np.arange(t_max)
         return t, vne.compute_von_neumann_entropy(self.diff_op, t_max=t_max)
 
-    def _calculate_eigendecomposition(self, tol=0.001):
+    def _calculate_eigendecomposition(self, tol=0.01):
         """Compute the partial eigendecomposition
 
         Parameters
@@ -1148,7 +1148,7 @@ class PHATE(BaseEstimator):
             explained_variance = np.cumsum(eigs_powered * density)
             explained_variance /= np.max(explained_variance)
             self._n_eigenvectors = int(
-                np.sum(density[np.cumsum(explained_variance) < 1-tol]).round()
+                np.sum(density[explained_variance < 1-tol]).round()
             )
             _logger.info(
                 "Using {} significant diffusion components".format(self._n_eigenvectors)