147aa3e39b8d3c6558cfd56ad924c4229097729d
Joao Felipe Rocha <joaofelipe.rocha@.yale.edu>
2025-09-18 15:48:52 -0400

    Major version updates. Deprecated nose2, updated requirements versions and added pyproject.toml

--- Python/test/test.py
+++ Python/test/test.py
@@ -8,7 +8,7 @@ import matplotlib
 
 matplotlib.use("Agg")  # noqa
 import scprep
-import nose2
+
 import os
 import phate
 import graphtools
@@ -17,19 +17,11 @@ import anndata
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 
-from nose.tools import assert_raises_regex, assert_warns_regex
 import re
 
+import pytest
 
-def assert_warns_message(expected_warning, expected_message, *args, **kwargs):
-    expected_regex = re.escape(expected_message)
-    return assert_warns_regex(expected_warning, expected_regex, *args, **kwargs)
-
-
-def assert_raises_message(expected_warning, expected_message, *args, **kwargs):
-    expected_regex = re.escape(expected_message)
-    return assert_raises_regex(expected_warning, expected_regex, *args, **kwargs)
-
+import warnings
 
 def test_simple():
     tree_data, tree_clusters = phate.tree.gen_dla(n_branch=3)
@@ -59,8 +51,8 @@ def test_simple():
     G = pygsp.graphs.Graph(G.W)
     phate_operator.fit(G)
     phate_operator.fit(anndata.AnnData(tree_data))
-    with assert_raises_message(
-        TypeError, "Expected phate_op to be of type PHATE. Got 1"
+    with pytest.raises(
+        TypeError, match="Expected phate_op to be of type PHATE. Got 1"
     ):
         phate.cluster.kmeans(1)
 
@@ -175,44 +167,40 @@ def test_bmmsc():
 
 def test_plot():
     tree_data, tree_clusters = phate.tree.gen_dla()
-    # scatter
-    assert_warns_message(
-        DeprecationWarning,
-        "Call to deprecated function (or staticmethod) scatter. (Use scprep.plot.scatter instead) -- Deprecated since version 1.0.0.",
-        phate.plot.scatter,
-        tree_data[:, 0],
-        tree_data[:, 1],
-        c=tree_clusters,
-        discrete=True,
-    )
-    # scatter2d
-    assert_warns_message(
-        DeprecationWarning,
-        "Call to deprecated function (or staticmethod) scatter2d. (Use scprep.plot.scatter2d instead) -- Deprecated since version 1.0.0.",
-        phate.plot.scatter2d,
-        tree_data,
-        c=tree_clusters,
-        discrete=True,
-    )
-    # scatter3d
-    assert_warns_message(
-        DeprecationWarning,
-        "Call to deprecated function (or staticmethod) scatter3d. (Use scprep.plot.scatter3d instead) -- Deprecated since version 1.0.0.",
-        phate.plot.scatter3d,
-        tree_data,
-        c=tree_clusters,
-        discrete=False,
-    )
-    # rotate_scatter3d
-    assert_warns_message(
-        DeprecationWarning,
-        "Call to deprecated function (or staticmethod) rotate_scatter3d. (Use scprep.plot.rotate_scatter3d instead) -- Deprecated since version 1.0.0.",
-        phate.plot.rotate_scatter3d,
-        tree_data,
-        c=tree_clusters,
-        discrete=False,
-    )
+    
+    # Test scatter with deprecation warning
+    with pytest.warns(DeprecationWarning, match="Call to deprecated function \\(or staticmethod\\) scatter\\. \\(Use scprep\\.plot\\.scatter instead\\) -- Deprecated since version 1\\.0\\.0\\."):
+        phate.plot.scatter(
+            tree_data[:, 0],
+            tree_data[:, 1],
+            c=tree_clusters,
+            discrete=True,
+        )
+    
+    # Test scatter2d with deprecation warning
+    with pytest.warns(DeprecationWarning, match="Call to deprecated function \\(or staticmethod\\) scatter2d\\. \\(Use scprep\\.plot\\.scatter2d instead\\) -- Deprecated since version 1\\.0\\.0\\."):
+        phate.plot.scatter2d(
+            tree_data,
+            c=tree_clusters,
+            discrete=True,
+        )
+    
+    # Test scatter3d with deprecation warning
+    with pytest.warns(DeprecationWarning, match="Call to deprecated function \\(or staticmethod\\) scatter3d\\. \\(Use scprep\\.plot\\.scatter3d instead\\) -- Deprecated since version 1\\.0\\.0\\."):
+        phate.plot.scatter3d(
+            tree_data,
+            c=tree_clusters,
+            discrete=False,
+        )
+    
+    # Test rotate_scatter3d with deprecation warning
+    with pytest.warns(DeprecationWarning, match="Call to deprecated function \\(or staticmethod\\) rotate_scatter3d\\. \\(Use scprep\\.plot\\.rotate_scatter3d instead\\) -- Deprecated since version 1\\.0\\.0\\."):
+        phate.plot.rotate_scatter3d(
+            tree_data,
+            c=tree_clusters,
+            discrete=False,
+        )
 
 
 if __name__ == "__main__":
-    exit(nose2.run())
+    pytest.main([__file__])
